091e540a3c70ae2cc69a3df3decbfdd18a0eb5f0
David de Rijke <d.p.derijke@gmail.com>
2021-09-22 11:32:07 +0200

    Add locale switcher

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="locale-switcher">
+  <select v-model="$i18n.locale">
+    <option
+        v-for="(lang, i) in languages"
+        :key="`Lang${i}`"
+        :value="lang">{{ lang }}</option>
+  </select>
+</div>
+</template>
+
+<script>
+export default {
+  name: "locale-switcher",
+  data(){
+    return { languages:['en', 'nl']}
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
