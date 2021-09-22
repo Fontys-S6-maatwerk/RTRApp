001289f1a42fc0189583dd61111ffa5a78115db2
David de Rijke <d.p.derijke@gmail.com>
2021-09-22 14:10:17 +0200

    move i18n files

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import vuetify from './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
-import i18n from './i18n'
+import i18n from './plugins/i18n'
 
 Vue.config.productionTip = false
 
