2eb8d719169b59ccb9cc372cba1eec0d0567950d
David de Rijke <d.p.derijke@gmail.com>
2021-09-22 11:32:24 +0200

    Update layout of i18n test component

--- src/views/I18nHome.vue
+++ src/views/I18nHome.vue
@@ -1,19 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <locale-switcher />
     <h1>following should be translated:</h1>
     <p>{{ $t('message.hello')}} </p>
   </div>
 </template>
 
 <script>
-import HelloWorld from "/src/components/HelloWorld";
+import LocaleSwitcher from "../components/LocaleSwitcher";
 
 export default {
   name: "I18nHome",
   components: {
-    HelloWorld
+    LocaleSwitcher
   }
 }
 
