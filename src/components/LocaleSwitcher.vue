85b13582b0c1644dbd68b14ad1e3254ac6c46737
David de Rijke <d.p.derijke@gmail.com>
2021-09-22 14:11:17 +0200

    upgrade locale-switcher to vuetify

--- src/components/LocaleSwitcher.vue
+++ src/components/LocaleSwitcher.vue
@@ -1,35 +1,32 @@
 <template>
 <div class="locale-switcher">
-  <select v-model="$i18n.locale">
-    <option
-        v-for="(lang, i) in languages"
-        :key="`Lang${i}`"
-        :value="lang"
-    > {{ lang }}</option>
-  </select>
+  <v-select
+      v-model=$i18n.locale
+      :items="languages()"
+      label="Select"
+  >
+  </v-select>
 </div>
 </template>
 
 <script>
 export default {
   name: "locale-switcher",
-  methods: {
-    getAvailableLocales() {
-      const locales = require.context('/src/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-      const availableLocales = {}
-      locales.keys().forEach(key =>{
-        const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-        if (matched && matched.length > 1) {
-          const locale = matched[1]
-          availableLocales[locale] = locale
-        }
-      })
-      return availableLocales
-    }
-  },
-  data() {
+  data () {
     return {
-      languages: this.getAvailableLocales()
+      languages: function () {
+        const locales = require.context('/src/locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+        const availableLocales = []
+        locales.keys().forEach(key => {
+          const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+          if (matched && matched.length > 1) {
+            const locale = matched[1]
+            availableLocales.push(locale)
+          }
+        })
+        console.log(availableLocales)
+        return availableLocales
+      }
     }
   }
 }
