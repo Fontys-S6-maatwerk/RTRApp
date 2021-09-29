d51180ac30c4195db7f26d7b303e7e87772142f8
jeetjeweet <jethrovanderkolk@gmail.com>
2021-09-29 12:32:34 +0200

    Update Solution.vue
    alle attributen van de solution staan nu op de pagina. moet wel nog mooi gemaakt worden en er moet nog materialen en instructions toegevoegd worden aan solutions.

--- src/views/Solution.vue
+++ src/views/Solution.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-    </div>
+<div>
+    <!-- Solution -->
+    <v-card elevation="1">
+        <v-card-title>{{ this.solution.name }}</v-card-title>
+        <div>Impact goal {{this.solution.impactGoal}}</div>
+        <div>{{this.solution.author}}</div>
+        <v-img src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+        <v-card-text>
+            <v-row align="center">
+               <div> {{ this.solution.numberOfLikes }} Likes </div>
+               <div> current impact {{ this.solution.currentImpact }} </div>
+               <div> Viewcount {{this.solution.viewCount}}</div>
+            </v-row>
+            <v-divider></v-divider>
+        </v-card-text>
+        <v-card-subtitle>{{this.solution.uploadDate}}</v-card-subtitle>
+        <v-card-title> {{this.solution.weatherExtremeType}} </v-card-title>
+    </v-card>
+    <!-- Reactions -->
+    <v-card elevation="1">
+        <v-card-title>Reactions</v-card-title>
+        <div v-for="reaction in this.solution.reactions" :key="reaction.id">
+            <div>{{reaction.id}}</div>
+        </div>
+        <!-- ?Laat hier elke reactie zien via een eigen view? -->
+    </v-card>
+</div>
 </template>
 
 <script>
@@ -18,6 +43,9 @@ export default {
             .then((response) => { 
                 this.solution = response.data;
             } );
+    },
+    methods:{
+
     }
 }
 </script>
