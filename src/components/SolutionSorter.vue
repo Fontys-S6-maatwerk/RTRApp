d4b92af9c3d61a7308d4de748700526e9d717189
Tjerk Sevenich <386599@student.fontys.nl>
2021-11-02 20:14:28 +0100

    ~UPDATE: solution sorting to emit chosen sorters

--- src/components/SolutionSorter.vue
+++ src/components/SolutionSorter.vue
@@ -1,59 +1,22 @@
 <template>
   <v-card class="ma-1">
-    <v-card-text class="px-2 d-flex justify-space-between">
-      <!-- Card solution sorter -->
-
-      <!-- filled -->
-      <!-- mdi-sort-calendar-descending -->
-
-      <!-- <v-select
-        v-for="sorter in sorters"
-        :key="sorter"
-        :append-outer-icon="sorter.items['ascending'].icon"
-        :label="sorter"
-        :items="sorter.items['ascending'].map((x) => x.text)"
-      ></v-select> -->
-
+    <v-card-text
+      :class="`pa-2 ${
+        $vuetify.breakpoint.smAndUp ? 'd-flex justify-space-between' : ''
+      }`"
+    >
       <v-select
+        class="pa-1"
         v-for="sorter in sorters"
-        :key="sorter"
+        :key="sorter.name"
+        :prepend-inner-icon="sorterIcon(sorter.name)"
         :menu-props="{ bottom: true, offsetY: true }"
-        :append-outer-icon="iconByText(sorter.name)"
         :label="sorter.name"
         :items="sorterItems(sorter.name)"
         item-text="text"
-        :v-model="sorterItems(sorter.name)[sorter.selected]"
-      ></v-select>
-
-      <!-- <v-select
-        :menu-props="{ bottom: true, offsetY: true }"
-        :append-outer-icon="iconByText('uploadDate')"
-        :label="sorters.uploadDate.name"
-        :items="sorterItems('uploadDate')"
-        item-text="text"
-        :v-model="sorterItems('uploadDate')[sorters.uploadDate.selected]"
-      ></v-select> -->
-
-      <!-- {{sorterItems('uploadDate')[0]}} -->
-
-      <!-- {{sorterItems('uploadDate')[0]}} -->
-      <!-- {{ sorters.uploadDate.selected }} -->
-      <!-- {{ sorterItems("uploadDate") }} -->
-
-      <!-- <v-select
-        append-outer-icon="mdi-sort-calendar-ascending"
-        label="Uploaddate"
-        :items="['Ascending', 'Descending']"
-        v-model="search"
-      ></v-select> -->
-
-      <v-select label="Relevance" :items="['Likes', 'Impact']"></v-select>
-
-      <v-select label="Views" :items="['Ascending', 'Descending']"></v-select>
-
-      <v-select
-        label="WeatherExtreme"
-        :items="['Tornado', 'Smog', 'Wildfire', 'Flood']"
+        hide-details
+        return-object
+        @change="filter"
       ></v-select>
     </v-card-text>
   </v-card>
@@ -63,110 +26,103 @@
 export default {
   data() {
     return {
-      sorters: {
-        uploadDate: {
-          name: "UploadDate",
+      sorters: [
+        {
+          name: "Upload date",
           selected: 0,
           items: [
             {
-              type: "ascending",
               text: "Ascending",
               icon: "mdi-sort-calendar-ascending",
             },
             {
-              type: "descending",
               text: "Descending",
               icon: "mdi-sort-calendar-descending",
             },
           ],
         },
-        Relevance: {
+        {
           name: "Relevance",
           selected: 0,
           items: [
             {
-              type: "likes",
               text: "Likes",
               icon: "mdi-thumb-up-outline",
             },
             {
-              type: "impact",
               text: "Impact",
               icon: "mdi-earth",
             },
           ],
         },
-        Views: {
+        {
           name: "Views",
           selected: 0,
           items: [
             {
-              type: "ascending",
               text: "Ascending",
               icon: "mdi-sort-numeric-ascending",
             },
             {
-              type: "descending",
               text: "Descending",
               icon: "mdi-sort-numeric-descending",
             },
           ],
         },
-        WeatherExtreme: {
-          name: "WeatherExtremes",
+        {
+          name: "Weather extremes",
           selected: 0,
           items: [
             {
-              type: "tornado",
               text: "Tornado",
               icon: "mdi-weather-tornado",
             },
             {
-              type: "smog",
               text: "Smog",
               icon: "mdi-smog",
             },
             {
-              type: "flood",
               text: "Flood",
               icon: "mdi-home-flood",
             },
             {
-              type: "wildfire",
               text: "Wildfire",
               icon: "mdi-fire",
             },
           ],
         },
-      },
+      ],
     };
   },
-  mounted() {
-    console.log(this.sorters);
-  },
   methods: {
-    sorterItems(sorter) {
-      return this.sorters[sorter].items;
-      //.map((x) => x.text);
-
-      //   x.entries.map(x => x);
-
-      //   .map(x => x)
+    sorterItems(sort) {
+      return this.sorters.find((x) => x.name === sort).items;
     },
-    iconByText(sorter) {
-      let selected = this.sorters[sorter].selected;
-      // console.log(this.sorters[sorter].items
-      // .filter((x) => x.type === selected).icon
-      // );
-      return this.sorters[sorter].items.filter((x) => x == selected).icon;
+    sorterIcon(sort) {
+      let sorter = this.sorters.find((x) => x.name === sort);
+
+      return sorter.items[sorter.selected].icon;
+    },
+    filter(item) {
+      //search for icon instead of text because of duplicate asc and desc text
+      let sorter = this.sorters.find((x) =>
+        x.items.map((x) => x.icon).includes(item.icon)
+      );
+
+      sorter.selected = sorter.items.indexOf(item);
+
+      let sorters = this.sorters.map((x) => {
+        return {
+          name: x.name,
+          sort: x.items[x.selected].text,
+        };
+      });
+
+      this.$emit("sort", sorters);
     },
   },
-  //sort by uploadldate, relevance(likes), views, by extreme
 };
 </script>
 
 <style scoped>
-.v-select {
-  max-width: 24%;
-}
 </style>
