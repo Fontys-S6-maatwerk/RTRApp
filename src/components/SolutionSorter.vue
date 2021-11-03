1491658272843b02c50cf90f29f20c53207a688a
jeetjeweet <jethrovanderkolk@gmail.com>
2021-11-03 13:21:54 +0100

    solution sorter copypasta, solutionlist add
    solution sorter copy pasta.
    Solution list er ingezet.

---
+++ src/components/SolutionSorter.vue
@@ -0,0 +1,120 @@
+<template>
+  <v-card class="ma-1">
+    <v-card-text
+      :class="`px-2 ${
+        $vuetify.breakpoint.smAndUp ? 'd-flex justify-space-between' : ''
+      }`"
+    >
+      <v-select
+        class="pa-1"
+        v-for="sorter in sorters"
+        :key="sorter.name"
+        :prepend-inner-icon="sorter.items[sorter.selected].icon"
+        :menu-props="{ bottom: true, offsetY: true }"
+        :label="sorter.name"
+        :items="sorter.items"
+        item-text="text"
+        hide-details
+        return-object
+        @change="filter"
+      ></v-select>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sorters: [
+        {
+          name: "Upload date",
+          selected: 0,
+          items: [
+            {
+              text: "Ascending",
+              icon: "mdi-sort-calendar-ascending",
+            },
+            {
+              text: "Descending",
+              icon: "mdi-sort-calendar-descending",
+            },
+          ],
+        },
+        {
+          name: "Relevance",
+          selected: 0,
+          items: [
+            {
+              text: "Likes",
+              icon: "mdi-thumb-up-outline",
+            },
+            {
+              text: "Impact",
+              icon: "mdi-earth",
+            },
+          ],
+        },
+        {
+          name: "Views",
+          selected: 0,
+          items: [
+            {
+              text: "Ascending",
+              icon: "mdi-sort-numeric-ascending",
+            },
+            {
+              text: "Descending",
+              icon: "mdi-sort-numeric-descending",
+            },
+          ],
+        },
+        {
+          name: "Weather extremes",
+          selected: 0,
+          items: [
+            {
+              text: "Tornado",
+              icon: "mdi-weather-tornado",
+            },
+            {
+              text: "Smog",
+              icon: "mdi-smog",
+            },
+            {
+              text: "Flood",
+              icon: "mdi-home-flood",
+            },
+            {
+              text: "Wildfire",
+              icon: "mdi-fire",
+            },
+          ],
+        },
+      ],
+    };
+  },
+  methods: {
+    filter(item) {
+      //search for icon instead of text because of duplicate asc and desc text
+      let sorter = this.sorters.find((x) =>
+        x.items.find((x) => x.icon === item.icon)
+      );
+
+      sorter.selected = sorter.items.indexOf(item);
+
+      let sorters = this.sorters.map((x) => {
+        return {
+          name: x.name,
+          sort: x.items[x.selected].text,
+        };
+      });
+
+      this.$emit("sort", sorters);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,40 +1,60 @@
 <template>
-  <v-form>
-    <v-container>
-      <v-row>
-        <v-col cols="12">
-          <v-textarea
-          v-model="searchMessage"
-          label="Search"
-          auto-grow
-          rows="1"
-          clearable
-          filled
-          :append-outer-icon="'mdi-send'"
-          :prepend-icon="'mdi-magnify'"
-          single-line
-          @click:append-outer="sendMessage"
-          @click:clear="clearMessage">
+<v-container>
+  <v-row>
+    <v-col cols="12">
+      <v-form>
+        <v-container>
+          <v-row>
+            <v-col cols="12">
+              <v-textarea
+              v-model="searchMessage"
+              label="Search"
+              auto-grow
+              rows="1"
+              clearable
+              filled
+              :append-outer-icon="'mdi-send'"
+              :prepend-icon="'mdi-magnify'"
+              single-line
+              @click:append-outer="sendMessage"
+              @click:clear="clearMessage">
 
-          </v-textarea>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-form>
+              </v-textarea>
+            </v-col>
+            <v-col cols="12">
+              
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-col>
 
+    <v-col cols="12">
+      <solutions-list
+      :solutions="solutions">
+
+      </solutions-list>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 
 <script>
-
+  import SolutionContext from "@/data/solution-context"
   export default  {
     name: 'src-views-search',
     props: [],
+    components: {
+      SolutionsList: () => import("@/components/SolutionsList.vue"),
+    },
     mounted () {
 
     },
     data () {
       return {
-        searchMessage: ''
+        searchMessage: '',
+        solutions: [],
+        solutionContext: new SolutionContext()
       }
     },
     methods: {
