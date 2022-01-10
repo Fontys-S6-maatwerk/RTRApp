087f2b2f36442ac9769ab2778c6a5120c58aa35b
Tjerk <47415892+Tjerk132@users.noreply.github.com>
2022-01-10 11:26:29 +0100

    Update CommentService.js

--- src/services/CommentService.js
+++ src/services/CommentService.js
@@ -1,32 +1,32 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://comment-service:80",
-  withCredentials: false, // This is the default
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+    baseURL: "http://localhost:9094",
+    withCredentials: false, // This is the default
+    headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+    },
 });
 
 export default {
-  async getSolutionComments(solutionId, pageNumber, pageSize) {
-    console.log("TEMP API CALL COMMENTS: ", solutionId, pageNumber, pageSize);
-    return await apiClient
-      .get(
-        "/comments/solutions/" +
-          solutionId +
-          "?pageSize=" +
-          pageSize +
-          "&pageNumber=" +
-          pageNumber
-      )
-      .then((response) => response.data);
-  },
+    async getSolutionComments(solutionId, pageNumber, pageSize) {
+        console.log("TEMP API CALL COMMENTS: ", solutionId, pageNumber, pageSize);
+        return await apiClient
+            .get(
+                "/comments/solutions/" +
+                solutionId +
+                "?pageSize=" +
+                pageSize +
+                "&pageNumber=" +
+                pageNumber
+            )
+            .then((response) => response.data);
+    },
 
-  async postComment(comment) {
-    return await apiClient
-      .post("/Comments", comment)
-      .then((response) => response.data);
-  },
+    async postComment(comment) {
+        return await apiClient
+            .post("/Comments", comment)
+            .then((response) => response.data);
+    },
 };
